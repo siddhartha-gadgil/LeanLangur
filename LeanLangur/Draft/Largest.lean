0a96b84d93f9c2903e61e8195cce916d2e9a74f2
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-12-14 14:21:55 +0000

    draft file

---
+++ LeanLangur/Draft/Largest.lean
@@ -0,0 +1,55 @@
+import Mathlib.Order.Lattice
+
+def largestNat (l: List Nat)  : Nat :=
+  match l with
+  | [] => 0
+  | [x] => x
+  | x :: y :: xs =>
+    Nat.max x (largestNat (y :: xs))
+
+theorem largestNat_mem (l: List Nat) (h: l ≠ []) :
+  largestNat l ∈ l := by
+  match l with
+  | [x] => simp [largestNat]
+  | x :: y :: xs =>
+    have ih := largestNat_mem (y :: xs) (by simp)
+    grind [largestNat]
+
+theorem largestNat_ge_all (l: List Nat) (h: l ≠ []) (x: Nat) :
+  x ∈ l → x ≤ largestNat l := by
+  match l with
+  | [y] =>
+    grind [largestNat]
+  | y :: z :: xs =>
+    have ih :=
+      largestNat_ge_all (z :: xs) (by simp) x
+    grind [largestNat]
+
+variable {α : Type}[LinearOrder α]
+
+def largest (l: List α) (h: l ≠ []) : α :=
+  match l with
+  | [] => by contradiction
+  | [x] => x
+  | x :: y :: xs =>
+    max x (largest (y :: xs) (by simp))
+
+theorem largest_mem (l: List α) (h: l ≠ []) :
+  largest l h ∈ l := by
+  match l with
+  | [x] => simp [largest]
+  | x :: y :: xs =>
+    have ih := largest_mem (y :: xs) (by simp)
+    simp [largest]
+    by_cases x ≤ largest (y :: xs)
+      (by simp) <;> grind
+
+theorem largest_ge_all (l: List α) (h: l ≠ []) (x: α) :
+  x ∈ l → x ≤ largest l h := by
+  match l with
+  | [y] =>
+    grind [largest]
+  | y :: z :: xs =>
+    have ih :=
+      largest_ge_all (z :: xs) (by simp) x
+    grind [largest]
--- LeanLangur/QuickSort.lean
+++ LeanLangur/QuickSort.lean
@@ -135,3 +135,55 @@ theorem quickSort_sorted (l : List α) : Sorted (quickSort l) := by
       quickSort_sorted (larger pivot l)
     apply sorted_sandwitch <;> grind
 termination_by l.length
+
+def largest (l: List α) (h: l ≠ []) : α :=
+  match l with
+  | [] => by contradiction
+  | [x] => x
+  | x :: y :: xs =>
+    max x (largest (y :: xs) (by simp))
+
+theorem largest_mem (l: List α) (h: l ≠ []) :
+  largest l h ∈ l := by
+  match l with
+  | [x] => simp [largest]
+  | x :: y :: xs =>
+    have ih := largest_mem (y :: xs) (by simp)
+    simp [largest]
+    by_cases x ≤ largest (y :: xs)
+      (by simp) <;> grind
+
+theorem largest_ge_all (l: List α) (h: l ≠ []) (x: α) :
+  x ∈ l → x ≤ largest l h := by
+  match l with
+  | [y] =>
+    grind [largest]
+  | y :: z :: xs =>
+    have ih :=
+      largest_ge_all (z :: xs) (by simp) x
+    grind [largest]
+
+def largestNat (l: List Nat)  : Nat :=
+  match l with
+  | [] => 0
+  | [x] => x
+  | x :: y :: xs =>
+    Nat.max x (largestNat (y :: xs))
+
+theorem largestNat_mem (l: List Nat) (h: l ≠ []) :
+  largestNat l ∈ l := by
+  match l with
+  | [x] => simp [largestNat]
+  | x :: y :: xs =>
+    have ih := largestNat_mem (y :: xs) (by simp)
+    grind [largestNat]
+
+theorem largestNat_ge_all (l: List Nat) (h: l ≠ []) (x: Nat) :
+  x ∈ l → x ≤ largestNat l := by
+  match l with
+  | [y] =>
+    grind [largestNat]
+  | y :: z :: xs =>
+    have ih :=
+      largestNat_ge_all (z :: xs) (by simp) x
+    grind [largestNat]
